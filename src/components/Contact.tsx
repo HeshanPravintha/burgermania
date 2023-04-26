3c75fc9c9d1de261853043885d4d079a5c62288f
HeshanPravintha <31812933+HeshanPravintha@users.noreply.github.com>
2023-04-26 14:27:16 +0530

    #

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import NavBarButton from './NavBarButton/NavBarButton';
-import Navbar from './Navbar';
+import React from "react";
+import NavBarButton from "./NavBarButton/NavBarButton";
+import Navbar from "./Navbar";
 
 const Contact: React.FC = () => {
   const handleButtonClick = () => {
-    console.log('Button clicked!');
-  }
+    console.log("Button clicked!");
+  };
   return (
     <div>
       <h1>Contact Us</h1>
@@ -14,6 +14,6 @@ const Contact: React.FC = () => {
       <h1>Contact Us on LinkedIn</h1>
     </div>
   );
-}
+};
 
 export default Contact;
